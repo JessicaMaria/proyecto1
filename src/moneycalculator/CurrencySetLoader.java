e0035fc1ff2356bf7574eb6690eee2ba781b08d0
usuario <usuario@10.22.143.10>
2014-10-20 12:33:21 +0100

    Session 2_1

---
+++ src/moneycalculator/CurrencySetLoader.java
@@ -0,0 +1,14 @@
+package moneycalculator;
+
+public class CurrencySetLoader {
+
+    private final CurrencySet currencySet;
+
+    public CurrencySetLoader(CurrencySet currencySet) {
+        this.currencySet = currencySet;
+    }
+    
+    public void load(){
+        
+    }
+}
